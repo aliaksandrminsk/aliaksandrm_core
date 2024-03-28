0648b583625114d4b5db5ce660797450aaf8f1ce
DESKTOP-8N4GQGF\admin <gadzinik@gmail.com>
2024-03-28 16:43:22 +0300

    for test

--- src/utils/Textures.ts
+++ src/utils/Textures.ts
@@ -3,6 +3,13 @@ import * as PIXI from "pixi.js";
 export class Textures {
   static getTexture(id: string) {
     const loader = PIXI.Loader.shared;
+    // @ts-ignore: Unreachable code error//
+    //loader["test12"] = "medved";
+    // @ts-ignore: Unreachable code error
+    //console.error("AAAA Textures", (loader as any)["test12"]);
+    console.error("AAAA Textures TextureCache", PIXI.utils.TextureCache);
+
+
     if (loader.resources[id] == null) {
       console.error(
         `Error. id= `, id
